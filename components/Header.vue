95fe481270b77642aa0e1136dc1c57ca46822ea5
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2020-09-26 07:12:42 -0500

    Update to logo link

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,9 @@
 <div class="bg-gray-900">
     <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div class="relative flex items-center justify-between">
-            <a href="/" aria-label="Movies" title="Movies" class="inline-flex items-center">
+            <nuxt-link to="/" aria-label="Movies" title="Movies" class="inline-flex items-center">
                 <img class="h-5" src="https://media.graphcms.com/tniuSEbTRuua5FDqgEqn" />
-            </a>
+            </nuxt-link>
         </div>
     </div>
 </div>
